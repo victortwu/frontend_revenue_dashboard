e8889ce5109ea23222ef4b5278a1256ee8db339f
victortwu <victor607@gmail.com>
2021-05-23 23:14:31 -0700

    rendering bargraph sales now

--- dashboard-ui/src/components/BarGraph.js
+++ dashboard-ui/src/components/BarGraph.js
@@ -1,25 +1,47 @@
 import React from 'react'
 
 const BarGraph =(props)=> {
-console.log(props.reports)
+
+const doorDash = props.reports.filter(report=> report.vendor === 'Doordash')
+
+const uberEats = props.reports.filter(report=> report.vendor === 'UberEats')
+
+const postmates = props.reports.filter(report=> report.vendor === 'Postmates')
+
+const grubhub = props.reports.filter(report=> report.vendor === 'Grubhub')
+
+const doorDashTotal = doorDash.map(item=> {
+  let ddItem = item.subtotal
+  let no$ = ddItem.replace('$', '')
+  let number = parseFloat(no$)
+  return number
+}).reduce((acc, curr) => acc + curr, 0)
+console.log(doorDashTotal)
+
+const uberEatsTotal = uberEats.map(item=> {
+  return item.subtotal
+}).reduce((acc, curr) => acc + curr, 0)
+console.log(uberEatsTotal)
+
+const postmatesTotal = postmates.map(item=> {
+  return item.subtotal
+}).reduce((acc, curr) => acc + curr, 0)
+console.log(postmatesTotal)
+
+const grubhubTotal = grubhub.map(item=> {
+  return item.subtotal
+}).reduce((acc, curr) => acc + curr, 0)
+console.log(grubhubTotal)
+
 
   return(
     <>
-      {
-        props.reports.map(report=> {
-
-            return <li>{report.vendor}:  {report.subtotal}</li>
-
-          })
-
-      }
-
-
-
-
-
-
-
+      <ul>
+        <li>DOORDASH SALES: ${doorDashTotal}</li>
+        <li>UBEREATS SALES: ----------${uberEatsTotal}</li>
+        <li>POSTMATES SALES: -------------${postmatesTotal}</li>
+        <li>GRUBHUB SALES: --------${grubhubTotal}</li>
+      </ul>
     </>
   )
 }
